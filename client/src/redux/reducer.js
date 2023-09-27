af5c3215b28f6f4aa78eab83a7acf999b449876e
IEchaburu <inaki.echaburu@hotmail.com>
2023-09-27 13:32:45 -0300

    reducer realizado

--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -1,10 +1,41 @@
+import { GET_PRODUCTS, GET_PRODUCT_NAME, GET_PRODUCT_ID, GET_PRODUCTS_CATEGORIES, CREATE_PRODUCT } from "./action-types";
+
+
+
 const initialState = {
-    items: [],
-    itemsCopy: []
+    products: [],
+    productsCopy: [],
+    categories: []
 }
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
+        case GET_PRODUCTS:
+            return {
+                ...state,
+                products: [action.payload],
+                productsCopy: [action.payload]
+            };
+        case GET_PRODUCTS_CATEGORIES:
+            return {
+                ...state,
+                categories: [action.payload]
+            };
+        case GET_PRODUCT_NAME:
+            return {
+                ...state,
+                products: [action.payload]
+            };
+        case GET_PRODUCT_ID:
+            return {
+                ...state,
+                products: [action.payload]
+            };
+        case CREATE_PRODUCT:
+            return {
+                ...state,
+                products: [action.payload]
+            };
         default:
             return {...state}
     }
