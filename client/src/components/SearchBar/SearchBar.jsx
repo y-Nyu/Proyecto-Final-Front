fb19ea8c351a57e0fa88723ddcd4bb8dd67fccb2
Nyu <rodry120s@gmail.com>
2023-09-29 12:47:22 -0300

    fin 1

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,9 +1,36 @@
-const SearchBar = () => {
-    return(
-        <div>
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  searchProducts,
+  searchUsers,
+  setSearchType,
+} from "../../redux/actions";
 
-        </div>
-    )
+function Searchbar(component) {
+  
+  const dispatch = useDispatch();
+  const searchType = useSelector((state) => state.searchType);
+  const [info, setInfo] = useState("");
+  dispatch(setSearchType(component));
+
+  const HandleChange = (event) => {
+    setInfo(() => {
+      const searched = event.target.value;
+      console.log(searchType);
+      if (searchType === "products") {
+        dispatch(searchProducts(searched));
+        return searched;
+      } else if (searchType === "users") {
+        dispatch(searchUsers(searched));
+        return searched;
+      }
+    });
+  };
+  return (
+    <div>
+      <input type="search" value={info} onChange={HandleChange} />
+    </div>
+  );
 }
 
-export default SearchBar;
+export default Searchbar;
