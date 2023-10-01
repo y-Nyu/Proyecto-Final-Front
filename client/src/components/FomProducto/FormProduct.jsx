e9007e93f338917beca09268b4632148fd5aded8
Melissa Monroy <melissamonroy@hotmail.com>
2023-10-01 02:51:36 -0300

    estilosProductForm

--- client/src/components/FomProducto/FormProduct.jsx
+++ client/src/components/FomProducto/FormProduct.jsx
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { ValidateProduct } from "../../Validate/Validate";
 import { getCategories } from "../../redux/Actions/Products/productsActions";
 import axios from "axios";
+import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 const FormProduct = () => {
   const dispatch = useDispatch();
@@ -22,10 +23,10 @@ const FormProduct = () => {
   });
   console.log(data);
   const [errors, setErrors] = useState({
-    name: "Ingese nombre menor a 20 caracteres",
-    brand: "Ingese marca menor a 20 caracteres",
+    name: "Ingrese nombre menor a 20 caracteres",
+    brand: "Ingrese marca menor a 20 caracteres",
     category: "Seleccione una categoria",
-    description: "Ingese detalle de producto mayor a 10 caracteres",
+    description: "Ingrese detalle de producto mayor a 10 caracteres",
     price: "Ingrese precio",
     stock: "Stock debe ser un número",
   });
@@ -91,46 +92,84 @@ const FormProduct = () => {
   };
 
   return (
-    <div>
-      <h1>Formulario ingreso Producto</h1>
+    <div className="container">
+      <div className="col">
+      <h2 className='fw-bold text-center pt-4'>Crear nuevo producto</h2>
       <form onSubmit={submitHandler}>
-        <label htmlFor="name">Name:</label>
-        <input type="text" name="name" onChange={handleChange} />
-        {errors.name ? <p>{errors.name}</p> : null}
-        <label htmlFor="brand">Brand</label>
-        <input type="text" name="brand" onChange={handleChange} />
-        {errors.brand ? <p>{errors.brand}</p> : null}
-        <label htmlFor="category">Category</label>
-        <select name="category" onChange={handleChange}>
-          {categories.map((category) => (
-            <option key={category.id} value={category.name}>
-              {category.name}
-            </option>
-          ))}
-        </select>
-        {errors.category ? <p>{errors.category}</p> : null}
-        <label htmlFor="description">description</label>
-        <input type="text" name="description" onChange={handleChange} />
-        {errors.description ? <p>{errors.description}</p> : null}
-        <label htmlFor="price">Price</label>
-        <input type="text" name="price" onChange={handleChange} />
-        {errors.price ? <p>{errors.price}</p> : null}
-        <label htmlFor="stock">Stock</label>
-        {/* <input type="text" name="stock" onChange={handleChange} />
-        {errors.stock ? <p>{errors.stock}</p> : null} */}
-        <label htmlFor="image">Image</label>
-        <input type="file" name="image" onChange={handleImageUpload} />
-        {data.image && (
-          <img src={data.image} alt={data.name} className="imagePreview" />
-        )}
+
+        <div className="mb-4 pt-4">
+          <label htmlFor="name" className="form-label">Nombre producto</label>
+          <input type="text" name="name" onChange={handleChange} className="form-control"/>
+          <div className="error-container">
+            {errors.name ? <p className={style["error-text"]}>{errors.name}</p> : <p className={style["error-text"]}></p>}
+          </div>
+        </div>
+        
+        <div className="mb-4 pt-4">
+          <label htmlFor="brand" className="form-label">Marca</label>
+          <input type="text" name="brand" onChange={handleChange} className="form-control"/>
+          <div className="error-container">
+            {errors.brand ? <p className={style["error-text"]}>{errors.brand}</p> : <p className={style["error-text"]}></p>}
+          </div>
+        </div>
+        
+        <div className="mb-4 pt-4">
+          <label htmlFor="category" className="form-label">Categoria</label>
+          {/* PENDIENTE APLICAR ESTILOS DE BOOTSTRAP A LA LISTA DESPLEGABLE*/}
+          <select name="category" onChange={handleChange}>
+            {categories.map((category) => (
+              <option key={category.id} value={category.name}> {category.name} </option>
+            ))}
+          </select>
+          <div className="error-container">
+            {errors.category ? <p className={style["error-text"]}>{errors.category}</p> : <p className={style["error-text"]}></p>}
+          </div>
+        </div>
+        
+        <div className="mb-4 pt-4">
+          <label htmlFor="description" className="form-label">Descripción</label>
+          <input type="text" name="description" onChange={handleChange} className="form-control"/>
+          <div className="error-container">
+            {errors.description ? <p className={style["error-text"]}>{errors.description}</p> : <p className={style["error-text"]}></p>}
+          </div>
+        </div>
+
+        <div className="mb-4 pt-4">
+          <label htmlFor="price" className="form-label">Precio</label>
+          <input type="text" name="price" onChange={handleChange} className="form-control"/>
+          <div className="error-container">
+            {errors.price ? <p className={style["error-text"]}>{errors.price}</p> : <p className={style["error-text"]}></p>}
+          </div>
+        </div>
+
+        <div className="mb-4 pt-4">
+          <label htmlFor="stock" className="form-label">Stock / Inventario</label>
+          {/* <input type="text" name="stock" onChange={handleChange} className="form-control"/>
+          <div className="error-container">
+            {errors.stock ? <p className={style["error-text"]}>{errors.stock}</p> : <p className={style["error-text"]}></p>}
+          </div> */}
+        </div>
+
+        <div className="mb-4 pt-4">
+          {/* PENDIENTE APLICAR ESTILOS DE BOOTSTRAP*/}
+          <label htmlFor="image" className="form-label">Imagen</label>
+          <input type="file" name="image" onChange={handleImageUpload} className="form-control"/>
+          {data.image && (
+            <img src={data.image} alt={data.name} className="imagePreview" />
+          )}
+        </div>
+
+
         <div>
-          <button type="submit" disabled={!isFormValid()}>
-            Add Product
+          <button type="submit" disabled={!isFormValid()} className='btn btn-outline-primary w-100 my-1'>
+            Crear producto
           </button>
           <h2></h2>
         </div>
       </form>
+      </div>
     </div>
+      
   );
 };
 export default FormProduct;
