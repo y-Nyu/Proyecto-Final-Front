e361f0bbc422819ae9cadc768c44a9447796de02
FlorenciaHuespe <florenciahuespe1986@gmail.com>
2023-10-03 22:42:29 -0300

    se agrega estilo a la detail

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,7 +1,6 @@
-import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "./Navbar.css";
-import { useState } from "react";
 import imagelogo from "../../assets/logo/Logo.png";
 
 const NavBar = ({ userId, userImage }) => {
@@ -12,11 +11,11 @@ const NavBar = ({ userId, userImage }) => {
   const handleLogin = () => {
     loginState(!login);
   };
-  
+
   return (
     <div>
-      <nav className="navbar navbar-expand-md">
-        <div className="container-fluid">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container">
           <Link to="/" className="navbar-brand">
             <img
               src={imagelogo}
@@ -26,76 +25,86 @@ const NavBar = ({ userId, userImage }) => {
             />
           </Link>
 
-          <ul className="navbar-nav d-flex justify-content-center align-items-center">
-            <li className="nav-item">
-              <a className="nav-link" onClick={() => navigate("/")}>
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" onClick={() => navigate("/store")}>
-                Tienda
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" onClick={() => navigate("/about")}>
-                Sobre nosotros
-              </a>
-            </li>
-          </ul>
-          <div className={`d-flex ${login ? "" : "always-visible"}`}>
-            {login
-            ? (
-              <>
-                <button
-                  className="btn"
-                  onClick={() => {
-                    handleLogin();
-                    navigate("/loginRegister");
-                  }}
-                >
-                  Ingresar
-                </button>
-                <button
-                  className="btn"
-                  onClick={() => {
-                    handleLogin();
-                    navigate("/loginRegister");
-                  }}
-                >
-                  Registrarse
-                </button>
-              </>
-            ) : (
-              <>
-                <button className="btn cart always-visible" type="submit">
-                  
-                  {
-                    login 
-                    ? 
-                    <Link to={`/accountDetail/${userId}`} >
-                      (<img src={imagelogo} />)
-                    </Link>
-                    : 
-                    <Link to={`/accountDetail/${userId}`}>
-                      <i className="bi bi-person-circle custom-icon"></i>
-                    </Link>
-                  }
-                </button>
-                <button
-                  className="btn"
-                  onClick={() => {
-                    handleLogin();
-                    navigate("/loginRegister");
-                  }}
-                >
-                  Cerrar Sesión
-                </button>
-              </>
-            )}
-            <button className="btn cart" type="submit">
-              <i className="bi bi-cart"></i>
-            </button>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse justify-content-between" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link" onClick={() => navigate("/")}>
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" onClick={() => navigate("/store")}>
+                  Tienda
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" onClick={() => navigate("/about")}>
+                  Sobre nosotros
+                </a>
+              </li>
+            </ul>
+            <div className={`d-flex ${login ? "" : "always-visible"}`}>
+              {login ? (
+                <>
+                  <button
+                    className="btn btn-sm"
+                    onClick={() => {
+                      handleLogin();
+                      navigate("/loginRegister");
+                    }}
+                  >
+                    Ingresar
+                  </button>
+                  <button
+                    className="btn btn-sm"
+                    onClick={() => {
+                      handleLogin();
+                      navigate("/loginRegister");
+                    }}
+                  >
+                    Registrarse
+                  </button>
+                </>
+              ) : (
+                <>
+                  <button className="btn cart always-visible" type="submit">
+                    {login ? (
+                      <Link to={`/accountDetail/${userId}`}>
+                        (<img src={imagelogo} />)
+                      </Link>
+                    ) : (
+                      <Link to={`/accountDetail/${userId}`}>
+                        <i className="bi bi-person-circle custom-icon"></i>
+                      </Link>
+                    )}
+                  </button>
+                  <button
+                    className="btn btn-sm"
+                    onClick={() => {
+                      handleLogin();
+                      navigate("/loginRegister");
+                    }}
+                  >
+                    Cerrar Sesión
+                  </button>
+                </>
+              )}
+              <button className="btn cart" type="submit">
+                <i className="bi bi-cart"></i>
+              </button>
+            </div>
           </div>
         </div>
       </nav>
--- client/src/views/Detail/Detail.jsx
+++ client/src/views/Detail/Detail.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import "./Detail.css";
+import style from "./Detail.css";
 
 const Detail = () => {
   const { id } = useParams();
   const [productId, SetproductId] = useState({});
+  const [quantity, setQuantity] = useState(1); // Estado para la cantidad
 
   useEffect(() => {
     axios
@@ -14,26 +15,56 @@ const Detail = () => {
         SetproductId(data);
       })
       .catch((error) => {
-        alert("Error al obtener los detalles del producto:", error)
-      })
+        alert("Error al obtener los detalles del producto:", error);
+      });
   }, [id]);
+
+  // Función para aumentar la cantidad
+  const addToCart = () => {
+    // Aquí puedes realizar las acciones necesarias para agregar el producto al carrito
+    alert(`Se agregaron ${quantity} ${productId.name} al carrito`);
+  };
 
   return (
     <div className="container">
+        <div className="row align-items-center">
+        <div className="col-md-4">
+      <img
+        className="product-image img-fluid d-none d-md-block"
+        src={productId.image}
+        alt={productId.id}
+      />
+    </div>
+    <div className="col-md-8">
       <div className="card">
-        <h1 className="product-name">{productId.name}</h1>
-        <img
-          className="product-image"
-          src={productId.image}
-          alt={productId.id}
-        />
-        <h3 className="product-brand">{productId.brand}</h3>
-        <h3 className="product-category">{productId.category}</h3>
-        <h3 className="product-description">{productId.description}</h3>
-        <h3 className="product-price">{productId.price}</h3>
+            <h1 className="product-name">{productId.name}</h1>
+            <h3 className="product-brand">{productId.brand}</h3>
+            <h3 className="product-category">{productId.category}</h3>
+            <h3 className="product-description">{productId.description}</h3>
+            <h3 className="product-price">
+              <strong>Precio: $ {productId.price}</strong>
+            </h3>
+
+            <div className="product-cart">
+              <button
+                onClick={addToCart}
+                className={`btn ${style.btn}`} 
+              >
+                Agregar al carrito
+              </button>
+              <input
+                type="number"
+                value={quantity}
+                onChange={(e) => setQuantity(e.target.value)}
+                min="1"
+                className="ml-2"
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 };
 
 export default Detail;
