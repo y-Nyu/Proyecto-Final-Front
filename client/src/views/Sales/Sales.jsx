181042f816ff705d8b80f05b31f4767fe3e8e4d4
Nyu <rodry120s@gmail.com>
2023-10-05 14:15:23 -0300

    jueves

--- client/src/views/Sales/Sales.jsx
+++ client/src/views/Sales/Sales.jsx
@@ -1,7 +1,43 @@
+import ah from "../../assets/aboutProfileImages/ah.jpg";
+
 const Sales = () => {
+    const compras = [
+        {
+            date: '05/10/2023',
+            compra: 'vslfnj23498ad',
+            total: 23.342,
+            name: 'Pelota',
+            unidades: 3,
+            imagen: ah
+        },
+        // {
+        //     date: '24/11/2024',
+        //     compra: 'lñajasfdasd234',
+        //     total: 10.424,
+        //     name: 'Comida',
+        //     unidades: 1,
+        //     imagen: ah
+        // }
+    ]
     return(
         <div>
-            <p>Este es el Sales</p>
+            <h1>Mis compras</h1>
+            <div className="card mb-3" >
+                <div className="row g-0">
+                    <div className="col-md-4">
+                    <img src="" alt="" className="img-fluid rounded-start"/>
+                    </div>
+                    <div className="col-md-8">
+                    <div className="card-body">
+
+                        <h5 className="card-title">{compras[0].name}</h5>
+                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p className="card-text"><small className="text-body-secondary">{compras[0].date}</small></p>
+                    </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
