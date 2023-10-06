cccce053eb4bde0b7fbb130da9927594f0a862f4
franco2730 <franco.rosales2130@gmail.com>
2023-10-05 23:09:32 -0300

    cart

---
+++ client/src/views/Cart/Cart.jsx
@@ -0,0 +1,96 @@
+import { useContext, useState, useEffect } from "react";
+import { CartContext } from "../../contexts/ShoppingCartContext";
+import "./Cart.css";
+import axios from "axios";
+
+const Cart = () => {
+  const [cart, setCart] = useContext(CartContext);
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  useEffect(() => {
+    // Calcular el precio total de todos los productos en el carrito
+    const newTotalPrice = cart.reduce(
+      (total, item) => total + item.price * item.quantity,
+      0
+    );
+    setTotalPrice(newTotalPrice); // Actualizar el estado
+  }, [cart]);
+
+  const incrementAmount = (productId) => {
+    const updatedCart = [...cart];
+    updatedCart.forEach((product) => {
+      if (product.id === productId) {
+        product.quantity += 1;
+      }
+    });
+    setCart(updatedCart);
+  };
+
+  const decrementAmount = (productId) => {
+    const updatedCart = [...cart];
+    updatedCart.forEach((product) => {
+      if (product.id === productId && product.quantity > 1) {
+        product.quantity -= 1;
+      }
+    });
+    setCart(updatedCart);
+  };
+
+  const removeItem = (productId) => {
+    const updatedCart = cart.filter((product) => product.id !== productId);
+    setCart(updatedCart);
+  };
+
+  const checkOut = async () => {
+    const { data } = await axios.post(
+      "https://pf-back-deploy.onrender.com/create-order",
+      cart
+    );
+    const initPoint = await data.body.init_point;
+    await cart.map(async (item) => {
+      let itemSale = {
+        iduser: 4,
+        idproduct: item.id,
+        quantity: item.quantity,
+      };
+      await axios.post("https://pf-back-deploy.onrender.com/sale", itemSale);
+    });
+
+    window.location.href = initPoint;
+    localStorage.clear();
+  };
+
+  return cart.length !== 0 ? (
+    <main className="contenedor">
+      <div className="carrito">
+        {cart.map((item) => (
+          <div className="item" key={item.id}>
+            <img src={item.image} alt={item.name} className="producto-imagen" />
+            <div className="producto-detalle">
+              <h2 className="producto-nombre">{item.name}</h2>
+              <p className="producto-precio">Precio c/u: ${item.price}</p>
+              <p className="producto-cantidad">Cantidad: {item.quantity}</p>
+              <button onClick={() => decrementAmount(item.id)}>-</button>
+              <span>{item.quantity}</span>
+              <button onClick={() => incrementAmount(item.id)}>+</button>
+              <button onClick={() => removeItem(item.id)}>Eliminar</button>
+              <p className="producto-total">
+                Total: ${item.price * item.quantity}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <button className="btn-pagar" onClick={checkOut}>
+        Pagar Total (${totalPrice})
+      </button>
+    </main>
+  ) : (
+    <div className="carrito-vacio">
+      <p>Oops, tu carrito está vacío</p>
+    </div>
+  );
+};
+
+export default Cart;
