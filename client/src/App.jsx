2bb539f133f9ffbdfabf789853a0d873b813b8ca
WalterHorst <whorst42@gmail.com>
2023-10-10 15:31:22 -0300

    hashRouter and succes component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,34 @@
-import NavBar from './components/NavBar/NavBar'
-import Footer from './components/Footer/Footer'
+import NavBar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer/Footer";
 import FormProduct from "./components/FomProducto/FormProduct";
-import FormUser from './components/FormUser/FormUser';
-import Home from './views/Home/Home'
-import About from './views/About/About'
-import LoginRegister from './views/LoginRegister/LoginRegister'
-import AccountDetail from './views/AccountDetail/AccountDetail'
-import Store from './views/Store/Store'
-import Detail from './views/Detail/Detail'
+import FormUser from "./components/FormUser/FormUser";
+import Home from "./views/Home/Home";
+import About from "./views/About/About";
+import LoginRegister from "./views/LoginRegister/LoginRegister";
+import AccountDetail from "./views/AccountDetail/AccountDetail";
+import Store from "./views/Store/Store";
+import Detail from "./views/Detail/Detail";
 import Faq from "./views/Faq/Faq";
 import Privacy from "./views/PrivacyP/Privacy";
-import Users from './views/Users/Users'
-import Sales from './views/Sales/Sales'
-import Cart from './views/Cart/Cart';
-import axios from 'axios';
-import { Routes, Route, useLocation, Navigate } from 'react-router-dom'
-import './App.css'
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { ShoppingCartProvider } from './contexts/ShoppingCartContext';
-import { getUserById } from './redux/Actions/Users/usersActions'
+import Users from "./views/Users/Users";
+import Sales from "./views/Sales/Sales";
+import Cart from "./views/Cart/Cart";
+import Success from "./components/Success/Success";
+import axios from "axios";
+import { Routes, Route, useLocation, Navigate } from "react-router-dom";
+import "./App.css";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { ShoppingCartProvider } from "./contexts/ShoppingCartContext";
+import { getUserById } from "./redux/Actions/Users/usersActions";
 import DashBoard from "./views/DashBoard/DashBoard";
-import jwtDecode from 'jwt-decode'
-import Stars from './components/Stars/Stars';
-
+import jwtDecode from "jwt-decode";
+import Stars from "./components/Stars/Stars";
 
 const App = () => {
-
   const location = useLocation();
-  const dispatch = useDispatch()
-  const token = sessionStorage.getItem("jwt_session")
+  const dispatch = useDispatch();
+  const token = sessionStorage.getItem("jwt_session");
 
   /*
   ME PARECE QUE EL CODIGO ACA ESTA AL REVES
@@ -46,18 +45,15 @@ const App = () => {
   */
 
   useEffect(() => {
-    
-    if(token)
-    {
+    if (token) {
       const decodedToken = jwtDecode(token);
       const userId = decodedToken.id;
       dispatch(getUserById(userId));
     }
-
-  }, [])
+  }, []);
 
   // ESTE CODIGO DE ACÁ ES PARA EL LOGIN DE GOOGLE
-  // 
+  //
   // DEBIDO A QUE EL POPUP DE GOOGLE NOS REDIRIGE ACÁ
   // CON PARAMETROS EN LA URL, LO QUE HAGO ES
   // CHEQUEAR SI EN ESTOS MISMOS PARAMETROS EXISTE EL CODIGO QUE
@@ -71,37 +67,33 @@ const App = () => {
   //
   // EN CASO CONTRARIO ARROJA UN ERROR
   useEffect(() => {
-
-    if(location.pathname == "/")
-    {
+    if (location.pathname == "/") {
       const queries = location.search;
       const params = new URLSearchParams(queries);
 
       let codeParam = params.entries().next();
-      while(!codeParam.done)
-      {
-        if(codeParam.value[0] == "code")
-        {
+      while (!codeParam.done) {
+        if (codeParam.value[0] == "code") {
           codeParam = codeParam.value[1];
           codeParam = decodeURI(codeParam);
-          
-          axios.post("https://pf-deploy-walterhorst.vercel.app/login-google", { google_code: codeParam})
-            .then(resp => resp.data)
-            .then(({id, token}) => {
-             
+
+          axios
+            .post("https://pf-deploy-walterhorst.vercel.app/login-google", {
+              google_code: codeParam,
+            })
+            .then((resp) => resp.data)
+            .then(({ id, token }) => {
               sessionStorage.setItem("jwt_session", token);
               dispatch(getUserById(id));
             })
-            .catch(error => alert(error.message));
+            .catch((error) => alert(error.message));
 
           break;
         }
       }
-
     }
-    
   }, [location]);
-  
+
   // NO FUNCIONA
   // const userRole = useSelector(state => state.userRole);
   // const navigate = useNavigate();
@@ -119,10 +111,10 @@ const App = () => {
 
   // // Esto es autenticación y autorización
   // useEffect(() => {
-   
+
   //   const token = sessionStorage.getItem("jwt_session");
   //   const isLogged = handleAuth(token);
-    
+
   //   // Chequea si el usuario está loggeado
   //   if(isLogged)
   //   {
@@ -139,29 +131,36 @@ const App = () => {
   return (
     <div>
       <ShoppingCartProvider>
-        <NavBar/>
+        <NavBar />
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/store" element={<Store/>}/>
-          <Route path="/detail/:id" element={<Detail/>}/>
-          <Route path="/about" element={<About/>}/>
-          <Route path="/loginRegister" element={token ? <Navigate to="/"/> : <LoginRegister/>}/>
-          <Route path="/accountDetail/:id" element={<AccountDetail/>}/>
-          <Route path="/cart" element={token ? <Cart/> : <Navigate to="/"/>}/>
+          <Route path="/success" element={<Success></Success>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/detail/:id" element={<Detail />} />
+          <Route path="/about" element={<About />} />
+          <Route
+            path="/loginRegister"
+            element={token ? <Navigate to="/" /> : <LoginRegister />}
+          />
+          <Route path="/accountDetail/:id" element={<AccountDetail />} />
+          <Route
+            path="/cart"
+            element={token ? <Cart /> : <Navigate to="/" />}
+          />
           {/* <Route path="/sales" element={token ? <Sales/> : <Navigate to="/"/>}/> */}
-          <Route path="/star" element={<Stars/>}/>
-          <Route path="/sales" element={<Sales/>}/>
-          <Route path="/preguntas-frecuentes" element={<Faq/>}/>
-          <Route path="/politica-de-privacidad" element={<Privacy/>}/>
-          <Route path='/formUser' element={<FormUser/>}></Route>
-          <Route path="/formProduct" element={<FormProduct/>}/>
-          <Route path="/admin" element={<DashBoard/>}/>
+          <Route path="/star" element={<Stars />} />
+          <Route path="/sales" element={<Sales />} />
+          <Route path="/preguntas-frecuentes" element={<Faq />} />
+          <Route path="/politica-de-privacidad" element={<Privacy />} />
+          <Route path="/formUser" element={<FormUser />}></Route>
+          <Route path="/formProduct" element={<FormProduct />} />
+          <Route path="/admin" element={<DashBoard />} />
           <Route path="/adminLogin" element={<LoginRegister />} />
           <Route path="/adminStore" element={<Store />} />
           <Route path="/adminUsers" element={<Users />} />
           <Route path="/adminSales" element={<Sales />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </ShoppingCartProvider>
     </div>
   );
