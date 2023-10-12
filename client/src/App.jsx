8ed36186cb615cbffb49fd5571327e5440aea3b8
Nyu <rodry120s@gmail.com>
2023-10-12 16:36:24 -0300

    deploy

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -73,20 +73,20 @@ const App = () => {
             
             codeParam = codeParam.value[1];
             codeParam = decodeURI(codeParam);
-
-        try {
-          const resp = await axios.post("https://pf-back-deploy.onrender.com/login-google", { google_code: codeParam });
-          const { id, name, email, rol, celular, token } = resp.data;
-
-          sessionStorage.setItem("jwt_session", token);
-          dispatch(createUserRole(rol));
-          dispatch(setUser({ id, email, name, rol, celular }));
-
-          // Redirige al usuario después de que se complete la autenticación
-          history.push("/ruta-después-del-inicio-de-sesión");
-        } catch (error) {
-          alert("ESTO ES UNA ALERTA DE ERROR: " + error);
-        }
+            
+            try {
+              const resp = await axios.post("https://pf-back-deploy.onrender.com/login-google", { google_code: codeParam });
+              const { id, name, email, rol, celular, token } = resp.data;
+            
+              sessionStorage.setItem("jwt_session", token);
+              dispatch(createUserRole(rol));
+              dispatch(setUser({ id, email, name, rol, celular }));
+            
+              // Redirige al usuario después de que se complete la autenticación
+              window.location("/");
+            } catch (error) {
+              alert("ESTO ES UNA ALERTA DE ERROR: " + error);
+            }
 
             break;
           }
