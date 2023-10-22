e25755b14606a0f8bfc4f2038274e13893deba93
Nyu <rodry120s@gmail.com>
2023-10-21 21:59:20 -0300

    deploy

---
+++ client/src/components/Profiles/Profiles.jsx
@@ -0,0 +1,41 @@
+import style from './Profile.module.css'
+
+const Profile = ( { id, name, github, linkedin, image } ) => {
+  return (
+    <div className="col-12 col-md-6 col-lg-3">
+    <div className={`proyecto ${style.proyecto}`}>
+        <img
+        className={`object-cover w-100 shadow-md ${style.image} ${style.smallImage}`} 
+        style={{ objectFit: "cover" }}
+        src={image}
+        alt={name}
+        />
+        <div className={`overlay ${style.overlay}`}>
+        <p
+            className={`text-center ${style.centeredParagraph} ${style.name}`}
+        >
+            {name}
+        </p>
+        <div className="iconos-contenedor text-center">
+            <a
+            href={github}
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            <i className="bi bi-github"></i>
+            </a>
+            <a
+            href={linkedin}
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            <i className="bi bi-linkedin"></i>
+            </a>
+        </div>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default Profile;
